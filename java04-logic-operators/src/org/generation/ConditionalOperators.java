e45064ac8d6d2969f76c02c4fc5e0a616288e93d
Sergio Torres <sergio.torres@idr.edu.mx>
2024-10-21 13:01:16 -0600

    feat: operadores condicionales

--- java04-logic-operators/src/org/generation/ConditionalOperators.java
+++ java04-logic-operators/src/org/generation/ConditionalOperators.java
@@ -3,7 +3,28 @@ package org.generation;
 public class ConditionalOperators {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/*
+		 * Los operadores evaluan valores booleanos
+		 * Tienen el comportamiento de corto circuito: El segundo operador
+		 *  solo se evalua si es necesario
+		 
+		   && conditional AND
+		   ||  conditonal OR
+		 
+		 */
+		
+        int value1 = 1;
+        int value2 = 2;
+        
+        if((value1 == 1) && (value2 == 2))
+            System.out.println("value1 is 1 AND value2 is 2"); // Se imprime
+        
+        if((value1 == 1) || (value2 == 1))
+            System.out.println("value1 is 1 OR value2 is 1"); // Se imprime
+        
+        
+        
+        
 
 	}
 
