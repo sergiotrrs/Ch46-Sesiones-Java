543066f55f573a50313c5e227da6f563cdfc3928
Sergio Torres <sergio.torres@idr.edu.mx>
2024-10-21 14:13:24 -0600

    feat: class scanner

--- java05-control-flow/src/org/generation/ConditionalIf.java
+++ java05-control-flow/src/org/generation/ConditionalIf.java
@@ -5,10 +5,9 @@ public class ConditionalIf {
 	public static void main(String[] args) {
 		/**
 		 * 
-		 * Flujos de control: las sentencias de flujo de control
-		 * interrumpen el flujo de ejecusión empleando toma
-		 * de decisiones, bucles y ramificaciones lo que permite
-		 * que el programa ejecute condicionalmente bloques de código
+		 * Flujos de control: las sentencias de flujo de control interrumpen el flujo de
+		 * ejecusión empleando toma de decisiones, bucles y ramificaciones lo que
+		 * permite que el programa ejecute condicionalmente bloques de código
 		 * particulares.
 		 * 
 		 * Sintaxis de la condicional if Si la condicional es verdadera, se ejecuta la
@@ -19,39 +18,37 @@ public class ConditionalIf {
 		 * if ( condicional ) { sentencias; }
 		 * 
 		 * if ( condicional ) sentencia; else sentencia_Si_condicional_es_falsa;
-		 * ------------------------------------------------------ 
-		 * if ( condicional )
-		 *   sentencia; 
-		 * else if ( nueva_condicional) 
-		 *   sentencia; 
-		 * else if ( n_condición ) 
-		 *   sentencia; 
-		 * else sentencia;
+		 * ------------------------------------------------------ if ( condicional )
+		 * sentencia; else if ( nueva_condicional) sentencia; else if ( n_condición )
+		 * sentencia; else sentencia;
 		 * 
 		 */
-		
-		
+
 		/*
 		 * Verifica la edad de una persona. Si es mayor o igual de 18 imprimir:
 		 * "Puede votar", caso contrario: "No puede votar"
 		 */
 		int age = 15;
-		if( age >= 18 ) System.out.println("Puede Votar");
-		else System.out.println("No puede votar");
-		
+		if (age >= 18)
+			System.out.println("Puede Votar");
+		else
+			System.out.println("No puede votar");
+
 		/*
 		 * De una variable tipo int, evaluar si está en el rango de 1 a 10, si es así
 		 * desplegar en consola "La selección está en el rango" En caso contrario
 		 * "La selección está fuera de rango"
 		 */
 		int myNum = 0;
-		if( myNum >= 1 && myNum <= 10  ) System.out.println("Está en el rango");
-		else System.out.println("Fuera de rango");
-		
-		
-		if( myNum == 15)/* sentencia */ ; // el ; termina la evaluación de if
+		if (myNum >= 1 && myNum <= 10)
+			System.out.println("Está en el rango");
+		else
+			System.out.println("Fuera de rango");
+
+		if (myNum == 15)
+			/* sentencia */ ; // el ; termina la evaluación de if
 		System.out.println("Número es igual"); // Se imprime siempre
-		
+
 		/*
 		 * Operador ternario.
 		 * 
@@ -59,12 +56,15 @@ public class ConditionalIf {
 		 *
 		 * Realizar el ejercicio anterior usado el operador ternario
 		 */
-		
+
 		// Refactorizar el ejercicio anterior usando el operador ternario
-		
-		
-		
-		
+
+		int evaluacionRango = 10;
+		System.out.println(
+				(evaluacionRango >= 1 && evaluacionRango <= 10) ? 
+						"Felicidades! estás en el rango" : 
+							"Fuera De Rango"
+							);
 
 	}
 
