3388785c433d684ec92f8a7ae08acc3ec97adf72
Sergio Torres <sergio.torres@idr.edu.mx>
2024-10-23 10:05:29 -0600

    feat: FoorLopp class

---
+++ java06-loop/src/org/generation/ForLoop.java
@@ -0,0 +1,20 @@
+package org.generation;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		/*
+		 * Sintaxis:
+		 * 
+		 * for( expresion_inicial ; comparacion; expresion_final) 
+		 * 	instrucción;
+		 * 
+		 * for( expresion_inicial ; comparacion; expresion_final){ 
+		 *      instrucciones; 
+		 *   }
+		 * 
+		 */
+
+	}
+
+}
