810f247786a10951f3850a1692d0acf7418e5601
Sergio Torres <sergio.torres@idr.edu.mx>
2024-10-28 11:00:52 -0600

    feat: sesion oop

---
+++ java09-OOP/src/com/santander/Atm.java
@@ -0,0 +1,42 @@
+package com.santander;
+
+
+/**
+ *  Clase: Una clase es una componente fundamental de la programación
+ *  orientada a objeto(OOP: Object oriented Programming). 
+ *  Una clase es una plantilla que define el
+ *  comportamiento(métodos) y las propiedades(atributos) de los objetos
+ *  que se crean(instancian) a partir de ella. Una clase puede
+ *  considerarse como un plano o un molde a partir del cual se crean
+ *  los objetos( instanciar la clase).
+ *  
+ */
+public class Atm {
+	/* Atributo de instancia (non-static fileds) 
+	 *   Variables que pertenecen a cada instancia (u objeto) de una clase. 
+	 *   Esto significa que cada objeto creado a partir de la clase tiene 
+	 *   su propia copia de estos atributos, por lo que pueden tener valores 
+	 *   diferentes en cada instancia.
+	 * 
+	 * */
+	int serialNumber;
+	String model;
+	double balance;
+	
+	/* Atributos de clase (static fields) 
+	 *   Atributos que pertenecen a la clase. Esto significa que 
+	 *   tendrá el mismo valor para todos los objetos. 
+	 *   Estos atributos son útiles para mantener información común en todos 
+	 *   los objetos. También, para propiedades donde no se requiere crear objetos.
+	 * 
+	 * */
+	static String bankName;
+	
+	/* Métodos constructores */
+	
+	/* Métodos de instancia (non-static methods) */
+	
+	/* Métodos de clase (static methods) */
+	
+
+}
---
+++ java09-OOP/src/com/santander/AtmTest.java
@@ -0,0 +1,23 @@
+package com.santander;
+
+public class AtmTest {
+
+	public static void main(String[] args) {
+		
+		// Instanciar la clase Atm y asignarlo a una variable
+		Atm cajero01 = new Atm();
+		// Instanciar nuevamente la clase Atm y asignarlo a la variable 'cajero02'
+		Atm cajero02 = new Atm();
+		
+		// Asignar valores a los atributos del cajero01
+		cajero01.serialNumber = 1;
+		cajero01.model = "T-800";
+		cajero01.balance =  450_000;
+		
+		// Asignar valores a los atributos del cajero02
+		
+		
+
+	}
+
+}
