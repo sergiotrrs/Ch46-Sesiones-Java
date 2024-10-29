4b7c93311c87ec708d560b150a4e806b8747276e
Sergio Torres <sergio.torres@idr.edu.mx>
2024-10-29 13:53:12 -0600

    feat: bloque de inicializacion

--- java10-encapsulacion/src/com/petco/Pet.java
+++ java10-encapsulacion/src/com/petco/Pet.java
@@ -2,27 +2,33 @@ package com.petco;
 
 class Dog {
 	
-	String name = "Solovino";
-	
-	Dog(){		
+	String name = "Solovino";	
+	{ // bloque de inicialización de atributos non-static
+		this.name = "Firulais";		
 	}
+	
+	Dog(){	}
 	
 	Dog( String name){
 		this.name = name;
 	}
 	
-} // Dog
+	Dog( String name, int age ){
+		this(name);
+		age++;
+	} 
+}// Dog
 
 
 public class Pet {
-	
 	public static void main(String[] args) {
 		Dog tessa = new Dog("Tessa");
 		System.out.println( tessa.name);	// Tessa
 		
 		Dog chocolate = new Dog();
-		System.out.println( chocolate.name); // Solovino
+		System.out.println( chocolate.name); // Firulais
 		
+		Dog locky = new Dog("Rocky", 9 );
+		System.out.println( locky.name );
 	}
-	
 } // Pet
