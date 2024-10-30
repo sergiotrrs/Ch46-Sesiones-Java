5fea91a169e2c85184e4da8d4a887a3a06639477
Sergio Torres <sergio.torres@idr.edu.mx>
2024-10-30 12:54:05 -0600

    feat: pruebas a clase cuadrado

--- java11-herencia/test/org/generation/shapes/SquareTest.java
+++ java11-herencia/test/org/generation/shapes/SquareTest.java
@@ -3,8 +3,20 @@ package org.generation.shapes;
 public class SquareTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Square cuadrito = new Square("Cuadrito", 4 );
+		// TwoDimensionalShape cuadrito = new Square("Cuadrito", 4 );
+		// Shape cuadrito = new Square("Cuadrito", 4 );
+		
+		System.out.println("Area:" + cuadrito.getArea() );
+		System.out.println("Perìmetro:" + cuadrito.getPerimeter() );
+		System.out.println("Lado:" + cuadrito.getWidth() );
+		
+		/*
+		 * Generar la clase concreta de trìangulo
+		 *  - Calcular el àrea y perìmetro
+		 *  - Hacer las pruebas
+		 */
+		
 	}
 
 }
