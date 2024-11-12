58feb5f10e70fac30b0eb68bb738e68b05571c75
Sergio Torres <sergio.torres@idr.edu.mx>
2024-11-12 13:39:55 -0600

    feat: spring JPA

---
+++ superneto-app/src/main/java/com/superneto/app/SupernetoAppApplication.java
@@ -0,0 +1,13 @@
+package com.superneto.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupernetoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupernetoAppApplication.class, args);
+	}
+
+}
---
+++ superneto-app/src/main/java/com/superneto/app/model/Customer.java
@@ -0,0 +1,94 @@
+package com.superneto.app.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+/**
+ *  @Entity se usa para marcar una clase como una entidad
+ *  que se puede persistir en una base de datos relacional.
+ *  
+ *  Se indica a JPA que la clase representa una tabla
+ *  en la base de datos y que los objetos de esta clase
+ *  puedan ser almacenados, actualizados o eliminados en 
+ *  dicha tabla. Además, cada instancia se la clase se considera
+ *  una fila en la tabla.
+ */
+@Entity
+public class Customer {
+	
+	// Indica que el atributo será la clave primaria
+	@Id 
+	// Indica como se generará automáticamente las claves primarias
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String fistName;
+	private String lastName;
+	
+	protected Customer() {}
+
+	protected Customer(String fistName, String lastName) {		
+		this.fistName = fistName;
+		this.lastName = lastName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFistName() {
+		return fistName;
+	}
+
+	public void setFistName(String fistName) {
+		this.fistName = fistName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", fistName=" + fistName + ", lastName=" + lastName + "]";
+	}
+	
+	
+	
+	
+
+}
+
+
+/*
+GenerationType.AUTO: Esta estrategia le permite al proveedor de JPA 
+elegir automáticamente la estrategia de generación más adecuada según 
+la base de datos y la configuración. El comportamiento exacto puede 
+variar dependiendo del proveedor utilizado.
+
+GenerationType.IDENTITY: Esta estrategia utiliza una columna de identidad 
+de la base de datos para generar automáticamente los valores de la clave primaria. 
+Es compatible con bases de datos como MySQL que admiten columnas de autoincremento. 
+En este caso, la base de datos se encarga de generar y asignar el valor de la clave primaria.
+
+GenerationType.SEQUENCE: Esta estrategia utiliza una secuencia de la base 
+de datos para generar automáticamente los valores de la clave primaria. 
+Se requiere que la base de datos admita secuencias. El proveedor de JPA 
+utiliza la secuencia definida en la base de datos para obtener los valores 
+de la clave primaria.
+
+GenerationType.TABLE: Esta estrategia utiliza una tabla especial en la base 
+de datos para generar los valores de la clave primaria. Se crea una tabla 
+separada que registra los valores generados para cada entidad. El proveedor 
+de JPA consulta esta tabla para obtener el próximo valor de la clave primaria.
+
+ */
