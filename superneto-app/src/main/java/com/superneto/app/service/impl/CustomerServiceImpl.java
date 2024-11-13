5894e77be28503af00f9bc848100da5313e944b8
Sergio Torres <sergio.torres@idr.edu.mx>
2024-11-13 13:48:07 -0600

    feat: getCustomerByID

--- superneto-app/src/main/java/com/superneto/app/service/impl/CustomerServiceImpl.java
+++ superneto-app/src/main/java/com/superneto/app/service/impl/CustomerServiceImpl.java
@@ -1,22 +1,69 @@
 package com.superneto.app.service.impl;
 
+import java.util.Optional;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.superneto.app.model.Customer;
+import com.superneto.app.repository.CustomerRepository;
 import com.superneto.app.service.CustomerService;
 
+/**
+ * La inyección de dependencias es un patrón de diseño que permite
+ * la creación de objetos y la gestión de sus dependencias de manera
+ * automática.
+ * 
+ * En lugar de crear los objetos directamente, la inyección de dependencias
+ * se encarga de proporcionar las dependencias necesarias a través de un
+ * contenedor de inversión de control.
+ * 
+ * La responsabilidad de crear y administrar los objetos se delega la contenedor
+ * de Spring.
+ * 
+ *  @Component es una anotación genérica que se utiliza
+*  para indicar que la clase es componente de Spring
+*  y debe ser gestionada por el contenedor
+*  de inversión de control (IoC).
+*  
+*  @Component se aplica a una clase para que Spring
+*  la detecte automáticamente y la registre como un bean
+*  en el contexto de la aplicación.
+*  
+*  @Qualifer se usa para resolver ambiguedades cuando
+*  hay varios objetos(beans) del mismo tipo en
+*  el contexto de la aplicación y se necesita especificar
+*  el bean que se debe inyectar.
+*  
+*  Bean: Es un objeto administrado por el contenedor de Spring.
+ */
 public class CustomerServiceImpl implements CustomerService {
+	
+	// @Autowired 1era forma de inyección de dependencia
+	CustomerRepository customerRepository;	
+	
+	// 2a forma de inyección de dependencia
+	public CustomerServiceImpl(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
 
 	@Override
 	public Customer createCustomer(Customer customer) {
-		// TODO Auto-generated method stub
-		return null;
+		// TODO Verificar si existe el email
+		// TODO Verificar si los atributos son válidos
+		Customer newCustomer = customerRepository.save( customer );
+		return newCustomer;
 	}
 
 	@Override
 	public Customer getCustomerById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<Customer> optionalCustomer = customerRepository.findById(id);
+		if( optionalCustomer.isEmpty()) {
+			// Lanzar una excepción.
+			throw new IllegalStateException("Customer does not exist with id " + id);
+		}
+		Customer existingCustomer = optionalCustomer.get();
+		return existingCustomer;
 	}
 
 	@Override
@@ -28,6 +75,7 @@ public class CustomerServiceImpl implements CustomerService {
 	@Override
 	public Set<Customer> getAllUsers(boolean isActive) {
 		// TODO Auto-generated method stub
+		// Envia mal los datos
 		return null;
 	}
 
@@ -44,3 +92,44 @@ public class CustomerServiceImpl implements CustomerService {
 	}
 
 }
+
+
+/**
+ * Una excepción es una situación no esperada
+ * durante la ejecución de un programa.
+ * 
+ * Existen dos tipos de excepciones.
+ * 
+ * A) Checked Exceptions: Son  excepciones que se heredan
+ * de la clase Exceptions. Se tiene que declarar la excepción
+ * en la firma del método o bien manejar la excepción con 
+ * el bloque try-catch.
+ * 
+ * B) Unchecked Exceptions: Son excepciones que se heredan
+ * de la clase RuntimeException. No se está obligado a procesarlas.
+ * Es opcional el uso de try-catch o declar en la firma del método.
+ * 
+ * Las excepciones de Error, son arrojadas por la JVM, son errores
+ * de los que no se pueden recuperar.
+ * 
+ * Manejo de excepciones con el bloque try-catch
+ * 
+ * Sintaxis:
+ * 
+ *  try{
+ *  
+ *  }
+ *  catch(exception e  ){
+ *  
+ *  }
+ * 
+ * try: bloque que contendrá sentencias que pudieran generar una excepción.
+ * catch: se ejecutará si ocurre una excepción en try
+ * finally: se ejecutará después de try o catch.
+ * 
+ * El stacktrace de una excepción es el conjunto
+ * de mensajes desde el origen de la excepción
+ * hasta la última clase que recibe la excepción.
+ * 
+ * 
+ */
