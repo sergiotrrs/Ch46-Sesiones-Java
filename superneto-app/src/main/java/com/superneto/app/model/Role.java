fd5cf94de51ccf48db890f891dcea324d982d013
Sergio Torres <sergio.torres@idr.edu.mx>
2024-11-14 15:55:04 -0600

    feat: entidad Role

---
+++ superneto-app/src/main/java/com/superneto/app/model/Role.java
@@ -0,0 +1,58 @@
+package com.superneto.app.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="roles")
+public class Role {
+	@Id 
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	@Column(name="name", length=50, nullable=false)
+	private String name;
+	@Column(name="description", length=150, nullable=false)
+	private String description;
+	
+	public Role() {}
+
+	public Role(String name, String description) {
+		this.name = name;
+		this.description = description;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public String toString() {
+		return "Role [id=" + id + ", name=" + name + ", description=" + description + "]";
+	}
+	
+	
+}
