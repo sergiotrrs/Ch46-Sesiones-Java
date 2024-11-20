faabaa024f913d502d03be1e21a96290f6a44d41
Sergio Torres <sergio.torres@idr.edu.mx>
2024-11-20 11:27:53 -0600

    feat: customer login controller

--- superneto-app/src/main/java/com/superneto/app/controller/LoginCustomerController.java
+++ superneto-app/src/main/java/com/superneto/app/controller/LoginCustomerController.java
@@ -1,12 +1,26 @@
 package com.superneto.app.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import com.superneto.app.model.Customer;
+import com.superneto.app.service.LoginCustomerService;
 
 @RestController
 @RequestMapping("api/v1/login")
 @CrossOrigin(origins = "*")
 public class LoginCustomerController {
 
-	
+	LoginCustomerService loginCustomerService;
 
+	public LoginCustomerController(LoginCustomerService loginCustomerService) {	
+		this.loginCustomerService = loginCustomerService;
+	}
+	
+	@PostMapping
+	ResponseEntity<Customer> loginUser(@RequestBody Customer customer){
+		Customer existingCustomer =  loginCustomerService.loginCustomer(customer);
+		return ResponseEntity.ok(existingCustomer);
+	}
+	
 }
